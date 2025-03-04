6a5beddeb3c5f1158d8a4c263655964884ee3bfd
captainsage <miklos.z.regenhart@gmail.com>
2025-03-04 19:16:11 +0100

    Title scene done

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    public void StartMainApp()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
